c3199764ab39e1765b5edf230cf34decbc9935d0
danteBenitez <benitezdante123@gmail.com>
2023-06-21 14:46:11 -0300

    Configurado middleware, variables de entorno y página 404

---
+++ README.md
@@ -0,0 +1,34 @@
+# Segundo Parcial de Taller de Lenguaje de programación
+
+## Tema: Reserva de vuelos para un aeropuerto
+
+![](https://2.bp.blogspot.com/-PN2l1DFJqZs/WpmEnvtjN6I/AAAAAAAAAS4/p1VpMOOAsug-eYMU_GOUXJE-bWGq9mCnQCLcBGAs/w1200-h630-p-k-no-nu/El%2BAeropuerto%2By%2Blos%2BServicios%2BAeroportuarios.jpg)
+
+### Instalación del proyecto
+
+Para instalar el proyecto de modo local: 
+
+- Clone el repositorio 
+
+```bash
+git clone https://github.com/danteBenitez/tlp1-segundo-parcial.git
+```
+
+- Dentro de la carpeta creada por el comando anterior, instale las dependecias del proyecyto
+
+```bash	
+npm install
+```
+
+- Cree una copia del archivo .env.example
+
+- Renóbrela como .env
+
+- Complete con las variables de entorno necesarias. Deberá tener una base de datos corriendo y colocar sus credenciales en tal archivo.
+
+- Ejecute
+
+```bash
+npm run dev
+```
+
--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 // Imports
-const cors = require('cors');
 const express = require('express');
-
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+require('dotenv').config();
 const path = require('path');
 
 const app = express();
 
 // Middlewares
 // TODO: Implementar middlewares
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
 
 // Routes
 app.use('/api', require('./routes/reserva.routes'));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+app.use(function(_req, res, _next) {
+    res.status(404).render('404');
+})
+
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(process.env.PORT, () => console.log(`Server on port ${process.env.PORT}`));
